18f22ed33e377194559b0e9ef0f6a3a6ce3216aa
homura <homura.dev@gmail.com>
2020-08-12 17:36:00 +0800

    feat: update to new version muta sdk

--- src/commands/repl.ts
+++ src/commands/repl.ts
@@ -1,21 +1,24 @@
-import {Command, flags} from '@oclif/command'
+import { Command, flags } from '@oclif/command';
 
 export default class REPL extends Command {
-  static description = 'muta client REPL'
+  static description = 'muta client REPL';
 
   static flags = {
-    help: flags.help({char: 'h'}),
+    help: flags.help({ char: 'h' }),
     // flag with a value (-n, --name=VALUE)
     // name: flags.string({char: 'n', description: 'name to print'}),
     // flag with no value (-f, --force)
     // force: flags.boolean({char: 'f'}),
-    endpoint: flags.string({char: 'e', description: 'chain endpoint to connect'}),
-  }
+    endpoint: flags.string({
+      char: 'e',
+      description: 'chain endpoint to connect',
+    }),
+  };
 
-  static args = []
+  static args = [];
 
   async run() {
-    const {args, flags} = this.parse(REPL);
+    const { flags } = this.parse(REPL);
 
     // const name = flags.name || 'world'
     // this.log(`hello ${name} from ./src/commands/hello.ts`)
@@ -23,30 +26,32 @@ export default class REPL extends Command {
     //   this.log(`you input --force and --file: ${args.file}`)
     // }
     // process.env.NODE_OPTIONS = '--experimental-repl-await';
-    const repl = require("repl");
-    const vm = require("vm");
-    const { processTopLevelAwait } = require("node-repl-await");
+    const repl = require('repl');
+    const vm = require('vm');
+    const { processTopLevelAwait } = require('node-repl-await');
 
     function isRecoverableError(error) {
-        if (error.name === 'SyntaxError') {
-            return /^(Unexpected end of input|Unexpected token)/.test(error.message);
-        }
-        return false;
+      if (error.name === 'SyntaxError') {
+        return /^(Unexpected end of input|Unexpected token)/.test(
+          error.message
+        );
+      }
+      return false;
     }
 
     async function myEval(code, context, filename, callback) {
-        code = processTopLevelAwait(code) || code;
+      code = processTopLevelAwait(code) || code;
 
-        try {
-            let result = await vm.runInNewContext(code, context);
-            callback(null, result);
-        } catch (e) {
-            if (isRecoverableError(e)) {
-                callback(new repl.Recoverable(e));
-            } else {
-                console.log(e);
-            }
+      try {
+        const result = await vm.runInNewContext(code, context);
+        callback(null, result);
+      } catch (error) {
+        if (isRecoverableError(error)) {
+          callback(new repl.Recoverable(error));
+        } else {
+          console.log(error);
         }
+      }
     }
 
     const endpoint = flags.endpoint || 'http://127.0.0.1:8000/graphql';
@@ -57,9 +62,13 @@ export default class REPL extends Command {
     c.lodash = lodash;
     c.muta_sdk = require('@mutadev/muta-sdk');
     c.client = new c.muta_sdk.Client({ endpoint });
-    const mnemonic = c.muta_sdk.Muta.hdWallet.generateMnemonic();
-    c.wallet = new c.muta_sdk.Muta.hdWallet(mnemonic);
-    c.accounts = lodash.range(20).map(i => c.wallet.deriveAccount(i));
+    const { HDWallet } = await import('@mutadev/wallet');
+
+    const mnemonic = HDWallet.generateMnemonic();
+    const wallet = new HDWallet(mnemonic);
+    // high security risk
+    // c.wallet = wallet;
+    c.accounts = lodash.range(20).map((i: number) => wallet.deriveAccount(i));
     c.service = service;
   }
 }
